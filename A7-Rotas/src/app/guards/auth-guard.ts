a4ba7d815623986dce4e83742fe4caf189b3816f
Edgar Giocondo Gabbo <edgar.gabbo@corp.nova>
2018-02-28 08:03:19 -0300

    rotas

--- A7-Rotas/src/app/guards/auth-guard.ts
+++ A7-Rotas/src/app/guards/auth-guard.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad, Route } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
 import { AuthService } from '../login/auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
+
+  canLoad(route: Route): boolean | Observable<boolean> | Promise<boolean> {
+    return this.verificarAcesso();
+  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    return this.verificarAcesso();
+  }
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  private verificarAcesso() {
     if (this.authService.usuarioEstaAutenticado()) {
       return true;
     }
@@ -16,10 +29,5 @@ export class AuthGuard implements CanActivate {
 
     return false;
   }
-
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) { }
 
 }
